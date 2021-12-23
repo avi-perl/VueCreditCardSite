59779b9f453c0860884c830b8daa88f3bead6b68
Avrohom Perl <webmaster@lomdei.com>
2021-12-22 20:31:16 -0500

    Init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CreditCardList :cards="cards" :card_count="cardCount" />
+  <AddCreditCard msg="Welcome to Your Vue.js App Avi" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddCreditCard from "./components/AddCreditCard.vue";
+import CreditCardList from "./components/CreditCardList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AddCreditCard,
+    CreditCardList,
+  },
+  data: function () {
+    return {
+      cards: [
+        {
+          id: 1,
+          name: "Avi Doe",
+          number: "1111-2222-3333-4444",
+          expiry: {
+            month: "01",
+            year: "18",
+          },
+          cvv: "352",
+          selected: true,
+          redacted: false,
+        },
+        {
+          id: 2,
+          name: "Jane Perl",
+          number: "1111-2222-3333-4444",
+          expiry: {
+            month: "05",
+            year: "17",
+          },
+          cvv: "352",
+          selected: false,
+          redacted: false,
+        },
+        {
+          id: 3,
+          name: "John Doe",
+          number: "1111-2222-3333-4444",
+          expiry: {
+            month: "01",
+            year: "18",
+          },
+          cvv: "352",
+          selected: false,
+          redacted: true,
+        },
+      ],
+    };
+  },
+  computed: {
+    cardCount: function () {
+      return this.cards.length;
+    },
+  },
+  methods: {
+    selectCard: function (card) {
+      this.cards.forEach(function (c) {
+        c.selected = false;
+      });
+      this.cards[card.id - 1].selected = true;
+      console.log(this.cards[card.id - 1]);
+    },
+    deleteCard: function (cardId) {
+      this.cards = this.cards.filter(card => card.id !== cardId);
+    },
+    addCard: function (cardData) {
+      let newCard = {
+        id: this.cardCount + 1,
+        name: cardData.name || "",
+        number: cardData.number || "",
+        expiry: {},
+        cvv: cardData.cvv || "",
+        selected: false,
+        redacted: false,
+      }
+      newCard.expiry.month = cardData.expirymonth || "";
+      newCard.expiry.year = cardData.expiryyear || "";
+      this.cards.push(newCard);
+      this.selectCard(this.cardCount);
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,8 +95,14 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+
+@font-face {
+  font-family: OCRA;
+  src: local("OCRA"),
+    url("./assets/fonts/OCRA.ttf") format("truetype");
 }
 </style>
---
+++ src/components/AddCreditCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <form @submit.prevent="submit" class="col-50">
+    <h3>Payment</h3>
+    <br />
+    <label for="fname">Accepted Cards</label>
+    <div class="icon-container">
+      <i class="fa fa-cc-visa" style="color: navy"></i>
+      <i class="fa fa-cc-amex" style="color: blue"></i>
+      <i class="fa fa-cc-mastercard" style="color: red"></i>
+      <i class="fa fa-cc-discover" style="color: orange"></i>
+    </div>
+    <label for="cname">Name on Card</label>
+    <input type="text" id="cname" name="cardname" placeholder="John More Doe" v-model="name"/>
+    <label for="ccnum">Credit card number</label>
+    <input
+      type="text"
+      id="ccnum"
+      name="cardnumber"
+      placeholder="1111-2222-3333-4444"
+      v-model="number"
+    />
+    <label for="expmonth">Exp Month</label>
+    <input type="text" id="expmonth" name="expmonth" placeholder="September" v-model="expirymonth"/>
+    <div class="row">
+      <div class="col-50">
+        <label for="expyear">Exp Year</label>
+        <input type="text" id="expyear" name="expyear" placeholder="2018" v-model="expiryyear"/>
+      </div>
+      <div class="col-50">
+        <label for="cvv">CVV</label>
+        <input type="text" id="cvv" name="cvv" placeholder="352" v-model="cvv"/>
+      </div>
+    </div>
+    <button type="submit">SUBMIT</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddCreditCard",
+  props: {
+    msg: String,
+    items: Array,
+  },
+  methods: {
+    submit: function () {
+      console.log(this.number);
+      this.$root.addCard(this);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
